a4e1907d9e7b215185025ef1cec0ea172c0299a9
Adham Nour <adhamnourelwaffaa@gmail.com>
2023-08-27 02:28:49 +0300

    finished the first exsecise in the GPIO

--- GPIO_Ex1/GPIO_Ex1.c
+++ GPIO_Ex1/GPIO_Ex1.c
@@ -5,15 +5,38 @@
  *      Author: adham
  */
 
-
 #include "avr/io.h"
 #include "util/delay.h"
 
-int main(void){
-	DDRB|=1<<0;
-	PORTB&=~(1<<0);
-	while(1){
-		PORTB^=(1<<0);
-		_delay_ms(500);
+void setBit(unsigned char *x, unsigned char bitNumber);
+void clearBit(unsigned char *x, unsigned char bitNumber);
+void toggleBit(unsigned char *x, unsigned char bitNumber);
+unsigned char checkBit(unsigned char *x,unsigned char bitNumber);
+
+int main(void) {
+	setBit(&DDRC, DDC1); //seting the pin zero as output pin
+	clearBit(&PORTC, PC1); // initial output to be zero
+	clearBit(&DDRD,DDD2);// setting the second pin to be input pin
+
+	while (1) {
+		if(!checkBit(&PIND,PIND2)){
+			setBit(&PORTC, PC1);
+		}
+		else{
+			clearBit(&PORTC, PC1);
+		}
 	}
+}
+void setBit(unsigned char *x, unsigned char bitNumber){
+	(*x)|=(1<<bitNumber);
+}
+
+void clearBit(unsigned char *x, unsigned char bitNumber){
+	(*x)&=~(1<<bitNumber);
+}
+void toggleBit(unsigned char *x, unsigned char bitNumber){
+	(*x)^=(1<<bitNumber);
+}
+unsigned char checkBit(unsigned char *x,unsigned char bitNumber){
+	return (*x)&(1<<bitNumber);
 }
